3b387b2dde47d89acdfe2b35bf2e34811668c9e4
Eve Schuetze <esc-berlin@outlook.de>
2023-06-18 22:59:13 +0200

    feat: platforms spawn properly on reset

--- scripts/game_screen.gd
+++ scripts/game_screen.gd
@@ -18,15 +18,8 @@ func _ready():
 	$HUD/RestartLabel.visible = true
 	$HUD/ScoreLabel.visible = false
 	
-	randomize()
-	platform_spawn = 0
-	while platform_spawn > -30000:
-		var new_platform = platform.instantiate()
-		var rand_platform_pos = randi_range(-width/2,width/2)
-		new_platform.set_position(Vector2(rand_platform_pos,platform_spawn))
-		add_child(new_platform)
-		platform_spawn -= randi_range(20,120)
-	
+	spawn_platforms()
+
 func _process(delta):
 	$HUD/RestartLabel.text = restartText % $RestartTimer.time_left
 	
@@ -34,10 +27,18 @@ func _process(delta):
 func _on_player_game_over():
 	$ScoreTimer.stop()
 	
+	#destroy all platforms
+	get_tree().call_group("platforms", "queue_free")
 	
 	$Player.canMove = false
-	$Player.position.x = $StartPosition.position.x
-	$Player.position.y = $StartPosition.position.y - 4 #TODO: remove - 4
+	$GameCamera.position = Vector2(0,0)
+	create_start_platform()
+	$Player.position.x = 0
+	$Player.position.y = 0 
+	
+	#recreate the platforms
+	spawn_platforms()
+	
 	$Player/AnimatedSprite2D.flip_h = false
 	$Player/AnimatedSprite2D.animation = "Idle"
 	$Player/AnimatedSprite2D.stop()
@@ -66,5 +67,24 @@ func _on_score_timer_timeout():
 func _on_restart_timer_timeout():
 	new_game()
 
+func create_start_platform():
+	var spawn_platform = platform.instantiate()
+	spawn_platform.position = Vector2(0,6)
+	add_child(spawn_platform)
 
+func spawn_platforms():
+	randomize()
+	platform_spawn = 0
+	while platform_spawn > -30000:
+		var new_platform = platform.instantiate()
+		var rand_platform_pos = randi_range(-width/2,width/2)
+		new_platform.set_position(Vector2(rand_platform_pos,platform_spawn))
+		
+		var randmove = randi_range(0,6)
+		if (randmove == 5):
+			new_platform.get_node("AnimationPlayer").play("horizontal_movement")
+		
+		add_child(new_platform)
+		
+		platform_spawn -= randi_range(20,90)
 
